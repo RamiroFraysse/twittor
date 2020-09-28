a4a089d0d3038112a9aebb5e4e73bba96247f101
Ramiro Fraysse <ramirofraysse@gmail.com>
2020-09-28 20:21:43 -0300

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE    = 'static-v3';
-const DYNAMIC_CACHE   = 'dynamic-v1';
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 //Corazon de la aplicacion, debe ser cargado lo mas rapido
@@ -57,6 +57,8 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then( keys => {
         keys.forEach( key => {
             if(key!== STATIC_CACHE && key.includes('static'))
+                return caches.delete(key);
+            if(key!== DYNAMIC_CACHE && key.includes('dynamic'))
                 return caches.delete(key);
         });
     });
